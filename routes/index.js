1243c266ad5047cf69bb23f2f9891544150749c6
calvintts <calvintts@mail.fresnostate.edu>
2018-06-19 13:46:09 -0700

    refactor

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../lib/User');
 var md5= require('md5');
 var mongoose = require('mongoose');
 var ExpressBrute = require ('express-brute');
