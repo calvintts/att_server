c85a6ef22013e0833151a4afc432e288dbaf1d77
calvintts <calvintts@mail.fresnostate.edu>
2018-06-19 15:22:07 -0700

    remove express-brute from index

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,10 @@ var express = require('express');
 var router = express.Router();
 var md5= require('md5');
 var mongoose = require('mongoose');
-var ExpressBrute = require ('express-brute');
-
-var store = new ExpressBrute.MemoryStore();
-var bruteforce = new ExpressBrute(store,{
-	freeRetries: 2,
-	minWait: 1000 * 60, //1 minute
-	maxWait: 10*60*1000, //10 minutes
-	failCallback: ExpressBrute.FailTooManyRequests
-});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hey Calvin Your API is working' });
+  res.send('hello calvin');
 });
 
 
